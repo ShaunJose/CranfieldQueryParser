c4c03e503d23e12bfaef3c2dec53e966e16fd1c0
ShaunJose <shaun.jose12@gmail.com>
2020-11-19 21:57:53 +0000

    Got queries
    just reusing methods :)

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -8,5 +8,7 @@ public class Constants
   public static final String[] FIELD_NAMES = {"title", "author", "background", "content"};
   public static final String[] FIELD_DELIMS = {".T\n", ".A\n", ".B\n", ".W\n"};
   public static final String INDEX_DIRECTORY = "index/";
+  public static final String CRAN_QRY_FILEPATH = "data/cran/cran.qry";
+  public static final String QUERY_DELIM = ".I [0-9]+\r\n.W\r\n";
 
 }
